4644fc756fe6a2b723c6ccd7a9f25d92c42786b7
Sallyvely <tpdus4159@naver.com>
2022-05-30 18:35:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ app.set('view engine','jade');
 app.set('views','./views');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false}));
+app.get('/login',function(req,res){
+	res.send('Login please');
+});
+app.listen(3000,function(){
+	console.log('Connected 3000 port!');
+});
