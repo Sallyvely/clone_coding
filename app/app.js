21c5b15ac781e4b9d46433459c11967c294b3716
Sallyvely <tpdus4159@naver.com>
2022-06-18 23:36:18 +0900

    apply nodemon

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@ const PORT = 3000;
 app.set("views","./src/views");
 app.set("view engine","ejs");
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/src'));
+
 const home = require("./src/routes/home");
 app. use("/",home); //미들 웨어를 등록해주는 메서드
 
