7777f8801e0cc5feb3e4359f6c81b7162ae4854a
Sallyvely <tpdus4159@naver.com>
2022-06-18 15:25:22 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//앱 세팅
+app.set("views","./src/views");
+app.set("view engine","ejs");
+
+app.use(express.static(__dirname + '/views'));
+const home = require("./src/routes/home");
+app. use("/",home); //미들 웨어를 등록해주는 메서드
+
+app.listen(PORT,function(){
+    console.log('conntected!')
+});
