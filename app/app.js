3f539b01a15c1d903715a7ab29a34f8876a5af93
Sallyvely <tpdus4159@naver.com>
2022-06-19 20:43:44 +0900

    modify style

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ const PORT = 3000;
 //앱 세팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use(express.static(__dirname + '/src'));
 
