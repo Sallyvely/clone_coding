f70776b575cccfb51d6f916f158466fdca3391b9
Sallyvely <tpdus4159@naver.com>
2022-07-21 22:30:27 +0900

    make story 5 accordian menu

--- app/src/index.js
+++
@@ -1,5 +0,0 @@
-$(".faq-question").click(function(){
-    $(this.next)(".faq-answer").stop().slideToggle(300);
-    $(this).toggleClass('on').siblings().removeClass('on');
-    $(this).next(".faq-answer").siblings(".faq-answer").slideUp(300);
-})
---
+++ app/src/views/home/index.js
@@ -0,0 +1,15 @@
+var que = document.getElementsByClassName("faq-question");
+var i;
+for (i=0; i < que.length; i++){
+    que[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementsSibling;
+        if(panel.style.display === "block"){
+            panel.style.display = "none";
+        }
+        else{
+            panel.style.display = "block";
+        }
+        
+    });
+}
