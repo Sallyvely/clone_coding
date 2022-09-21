0cfa9db56fa618936cce2e7b8889cfb6b476d502
Sallyvely <tpdus4159@naver.com>
2022-09-21 22:00:39 +0900

    make login function through fs module

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,16 +1,11 @@
 "use strict";
 
+const fs = require("fs").promises;
 
 process.setMaxListeners(15);
 class UserStorage{
-    static #users= {
-        id: ["sally", "admin", "seyeon"],
-        psword: ["1234", "1111", "123456"],
-        name: ["정세연", "어드민", "테스트"],
-     }
-
     static getUsers(...fields){
-        const users = this.#users;
+        //const users = this.#users;
         const newUsers = fields.reduce((newUsers, field)=>{
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -21,7 +16,17 @@ class UserStorage{
     }
 
     static getUserInfo(id){
-        const users = this.#users;
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) =>{
+                return this.#getUserInfo(data, id);
+
+            })
+            .catch(console.error);
+    };
+
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data);       
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users);
         const userInfo = usersKeys.reduce((newUsers,info)=>{
@@ -29,10 +34,9 @@ class UserStorage{
             return newUsers;
         },{});
         return userInfo;
-    };
-
+    }
     static save(userInfo){
-        const users = this.#users;
+        //const users = this.#users;
         users.id.push(userInfo.id);
         users.psword.push(userInfo.psword);
         console.log(users);
